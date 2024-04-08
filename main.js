51e264bd5194768e10426ad477e671646243068b
Bakhtawar <bakhtawar@codewithbakhtawar.com>
2024-04-09 00:57:12 +0500

    operators here

---
+++ main.js
@@ -0,0 +1,5 @@
+var math = 80;
+var english = 70;
+var total;
+total = ++math + math++ + --english + english-- + math + english++ + english;
+console.log(total);
---
+++ main.ts
@@ -0,0 +1,5 @@
+let math: number = 80;
+let english: number = 70;
+let total: number;
+total = ++math + math++ + --english + english-- + math + english++ + english;
+console.log(total);
